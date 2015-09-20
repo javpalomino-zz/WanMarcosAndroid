74f074b7bf828f6d7b7de3a71fe7ca980bd9a2fc
mrciezas <cieza15@hotmail.com>
2015-09-19 20:50:54 -0500

    probando http request

--- app/src/main/java/com/example/mox/myapplication/login.java
+++ app/src/main/java/com/example/mox/myapplication/login.java
@@ -6,16 +6,28 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class login extends AppCompatActivity implements View.OnClickListener {
 
     Button btn_login;
     EditText txt_email,txt_password;
+    RequestQueue requestQueue;
+    TextView mTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +37,26 @@ public class login extends AppCompatActivity implements View.OnClickListener {
         txt_email = (EditText) findViewById(R.id.txt_email);
         txt_password = (EditText) findViewById(R.id.txt_password);
         btn_login =  (Button) findViewById(R.id.btn_login);
+        mTextView = (TextView) findViewById(R.id.mTextView);
 
+        requestQueue = Volley.newRequestQueue(this);
+
+        String url ="http://192.168.1.103/proyectos/WanMarcosWEB/public/list";
+        
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.v(response,"salio");
+                mTextView.setText("Response is: "+ response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        requestQueue.add(stringRequest);
         btn_login.setOnClickListener(this);
     }
 
