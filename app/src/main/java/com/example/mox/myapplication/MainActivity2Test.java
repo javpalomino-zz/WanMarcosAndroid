a2feab4915c58de0eab81346f3f67565520e94c1
InkaTorque <fama1994@gmail.com>
2015-09-21 01:15:33 -0500

    primera prueba de vistas

---
+++ app/src/main/java/com/example/mox/myapplication/MainActivity2Test.java
@@ -0,0 +1,58 @@
+package com.example.mox.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity2Test extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    NavigationDrawerFragment drawerFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_test);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        drawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.SetUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout) , toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_activity2_test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.nextButton) {
+            startActivity(new Intent(this, SubActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+}
