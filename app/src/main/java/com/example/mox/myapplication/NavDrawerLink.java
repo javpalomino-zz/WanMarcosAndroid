c2d8370357e873bc01f2607e1173dea8690cecc0
InkaTorque <fama1994@gmail.com>
2015-09-27 02:16:55 -0500

    NavBar terminada

---
+++ app/src/main/java/com/example/mox/myapplication/NavDrawerLink.java
@@ -0,0 +1,11 @@
+package com.example.mox.myapplication;
+
+/**
+ * Created by Francisco on 26/09/2015.
+ */
+public class NavDrawerLink {
+
+    int iconId;
+    String title;
+
+}
--- app/src/main/java/com/example/mox/myapplication/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/mox/myapplication/NavigationDrawerFragment.java
@@ -2,21 +2,30 @@ package com.example.mox.myapplication;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NavigationDrawerFragment extends Fragment {
+public class NavigationDrawerFragment extends Fragment implements NavDrawerAdapter.ClickListener {
+
+    private RecyclerView recyclerView;
 
     public static final String PREF_FILE_NAME="testpref";
     public static final String KEY_USER_LEARNED_DRAWER="user_learned_drawer";
@@ -26,6 +35,8 @@ public class NavigationDrawerFragment extends Fragment {
 
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
+
+    private NavDrawerAdapter adapter;
 
     private View containerView;
     public NavigationDrawerFragment() {
@@ -46,9 +57,30 @@ public class NavigationDrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        View layout =inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+        adapter = new NavDrawerAdapter(getActivity(),getData());
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return layout;
     }
 
+
+    public static List<NavDrawerLink> getData()
+    {
+        List<NavDrawerLink> data=new ArrayList<>();
+        int[] icons = {R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher};
+        String[] titles={"Noticias","Docentes","Lugares","Eventos"};
+        for (int i=0;i<icons.length && i<titles.length;i++)
+        {
+            NavDrawerLink current = new NavDrawerLink();
+            current.iconId=icons[i];
+            current.title =titles[i];
+            data.add(current);
+        }
+        return data;
+    }
 
     public void SetUp(int fragmentId,DrawerLayout drawerLayout , Toolbar toolbar) {
         containerView = getActivity().findViewById(fragmentId);
@@ -94,5 +126,19 @@ public class NavigationDrawerFragment extends Fragment {
     public static String ReadFromPreferences(Context context, String preferenceName, String defaultValue){
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME , context.MODE_PRIVATE);
         return sharedPreferences.getString(preferenceName,defaultValue);
+    }
+
+    @Override
+    public void itemClicked(View view, int position) {
+        switch (position){
+            case 0 :startActivity(new Intent(getActivity(),SubActivity.class));break;
+            case 1:
+                Toast.makeText(getActivity(), "A Docentes", Toast.LENGTH_SHORT);break;
+            case 2:
+                Toast.makeText(getActivity(),"A Lugares",Toast.LENGTH_SHORT);break;
+            case 3:
+                Toast.makeText(getActivity(),"A Eventos",Toast.LENGTH_SHORT);break;
+        }
+
     }
 }
