5d20592146a89657c2350b58fcd3c70abb4c047d
mrciezas <cieza15@hotmail.com>
2015-10-01 17:44:57 -0500

    saving session user

--- app/src/main/java/wan/wanmarcos/activities/HomeActivity.java
+++ app/src/main/java/wan/wanmarcos/activities/HomeActivity.java
@@ -1,14 +1,18 @@
 package wan.wanmarcos.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import wan.wanmarcos.R;
+import wan.wanmarcos.utils.Constants;
 
 public class HomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +21,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
@@ -29,11 +33,20 @@ public class HomeActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id){
+            case R.id.action_settings : return true;
+            case R.id.logout : logout();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void logout(){
+        SharedPreferences preferences = getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor =  preferences.edit();
+        editor.clear();
+        editor.commit();
+        Intent login_activity = new Intent(this,MainActivity.class);
+        finish();
+        startActivity(login_activity);
     }
 }
