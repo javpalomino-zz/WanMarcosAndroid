6ff7c0b49d9737cf99948c4c02c48a49f0061fae
mrciezas <cieza15@hotmail.com>
2015-11-05 22:00:46 -0500

    checking layout contactanos and fixing bugs in login , scrollsize in actividtes login y contactanos

--- app/src/main/java/wan/wanmarcos/fragments/LoginFragment.java
+++ app/src/main/java/wan/wanmarcos/fragments/LoginFragment.java
@@ -18,9 +18,12 @@ import android.widget.TextView;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import org.json.JSONObject;
+
 import retrofit.Call;
 import retrofit.Callback;
 import retrofit.Response;
+import retrofit.Retrofit;
 import wan.wanmarcos.R;
 import wan.wanmarcos.activities.HomeActivity;
 import wan.wanmarcos.models.Error;
@@ -127,30 +130,44 @@ public class LoginFragment extends Fragment {
     }
 
     private void postLogIn(){
+
         Call<JsonElement> logInUser= restClient.getConsumerService().login(email, password,device_token, Constants.PLATFORM);
         String token;
         logInUser.enqueue(new Callback<JsonElement>() {
             @Override
             public void onResponse(Response<JsonElement> response) {
-                JsonObject responseBody = response.body().getAsJsonObject();
-                if(responseBody.has("token")){
-                    String token = responseBody.get("token").getAsString();
-                    setPreferences(token);
-                    changeToHome();
+                try {
+                    String message = "";
+                    if(response.isSuccess()){
+                        JsonObject responseBody = response.body().getAsJsonObject();
+                        lblError.setText(responseBody.toString());
+                        if(responseBody.has("token")){
 
-                }else{
-                    if(responseBody.has("error")){
-                        Error error = builder.buildError(responseBody.get("error").getAsJsonObject());
-                        lblError.setText(error.toString());
+                            String token = responseBody.get("token").getAsString();
+                            setPreferences(token);
+                            changeToHome();
+
+                        }else{
+                            if(responseBody.has("error")){
+                                Error error = builder.buildError(responseBody.get("error").getAsJsonObject());
+                                lblError.setText(error.toString());
+                            }else{
+
+                            }
+                        }
                     }else{
-
+                        JSONObject responseBody = new JSONObject(response.errorBody().string());
+                        message +=responseBody.getJSONObject("error").getString("message");
+                        message += responseBody.getString("error");
                     }
+                    lblError.setText(message);
+                }catch(Throwable e){
+                    lblError.setText(e.toString());
                 }
             }
-
             @Override
             public void onFailure(Throwable t) {
-
+                lblError.setText(t.toString());
             }
         });
     }
